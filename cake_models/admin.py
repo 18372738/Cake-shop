3dcdccb79b579b9e5c764d96b52a766e22830f69
18372738 <133884450+18372738@users.noreply.github.com>
2025-02-26 00:37:29 +0300

    Added models bitlink, filling the cake

--- cake_models/admin.py
+++ cake_models/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Bitlink, Sizes, Forms, Toppings, Berries, Decors
 
-# Register your models here.
+
+@admin.register(Bitlink)
+class BitlinkAdmin(admin.ModelAdmin):
+    list_display = ('original_url', 'bitlink', 'clicks')
+
+
+@admin.register(Sizes)
+class SizesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+
+
+@admin.register(Forms)
+class FormsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+
+
+@admin.register(Toppings)
+class ToppingsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+
+
+@admin.register(Berries)
+class BerriesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+
+
+@admin.register(Decors)
+class DecorsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
--- cake_models/models.py
+++ cake_models/models.py
@@ -1,3 +1,109 @@
 from django.db import models
 
-# Create your models here.
+
+class Bitlink(models.Model):
+    original_url = models.URLField(verbose_name="Url-Адрес")
+    bitlink = models.URLField(verbose_name="Сокращенная ссылка", blank=True, null=True)
+    clicks = models.IntegerField(verbose_name="Количество переходов", default=0)
+
+    class Meta():
+        verbose_name = 'Битлинк'
+        verbose_name_plural = 'Битлинки'
+
+    def __str__(self):
+        return self.bitlink or self.original_url
+
+
+class Sizes(models.Model):
+    title = models.CharField(
+        'Количество уровней',
+        max_length=100
+    )
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=7,
+        decimal_places=2,
+    )
+
+    class Meta():
+        verbose_name = 'Размер'
+        verbose_name_plural = 'Размеры'
+
+    def __str__(self):
+        return self.title
+
+
+class Forms(models.Model):
+    title = models.CharField(
+        'Форма',
+        max_length=100
+    )
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    class Meta():
+        verbose_name = 'Форма'
+        verbose_name_plural = 'Формы'
+
+    def __str__(self):
+        return self.title
+
+
+class Toppings(models.Model):
+    title = models.CharField(
+        'Топпинг',
+        max_length=100
+    )
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    class Meta():
+        verbose_name = 'Топпинг'
+        verbose_name_plural = 'Топпинги'
+
+    def __str__(self):
+        return self.title
+
+
+class Berries(models.Model):
+    title = models.CharField(
+        'Ягода',
+        max_length=100
+    )
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    class Meta():
+        verbose_name = 'Ягода'
+        verbose_name_plural = 'Ягоды'
+
+    def __str__(self):
+        return self.title
+
+
+class Decors(models.Model):
+    title = models.CharField(
+        'Декор',
+        max_length=100
+    )
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    class Meta():
+        verbose_name = 'Декор'
+        verbose_name_plural = 'Декоры'
+
+    def __str__(self):
+        return self.title
