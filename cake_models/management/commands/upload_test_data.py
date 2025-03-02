6c078599fcfec17033d7c567889da04fd276b0ba
LaryBary174 <lotuc74@gmail.com>
2025-03-02 13:35:16 +0500

    изменение загрузки тестовыми данными бд

--- cake_models/management/commands/upload_test_data.py
+++ cake_models/management/commands/upload_test_data.py
@@ -12,36 +12,36 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with transaction.atomic():
             # Создание клиентов
-            user_1 = User.objects.create(
-                phone="+79004217012",
-                username="Иван"
-            )
-            user_2 = User.objects.create(
-                phone="+79004216012",
-                username="Любовь"
-            )
-            user_3 = User.objects.create(
-                phone="+79004215012",
-                username="Пётр"
-            )
-            if not Profile.objects.filter(phone_number=user_1).exists():
-                profile_1 = Profile.objects.create(
-                    name=user_1.username,
-                    email="iv@mail.ru",
-                    phone_number=user_1
-                )
-            if not Profile.objects.filter(phone_number=user_2).exists():
-                profile_2 = Profile.objects.create(
-                    name=user_2.username,
-                    email="love@mail.ru",
-                    phone_number=user_2
-                )
-            if not Profile.objects.filter(phone_number=user_3).exists():
-                profile_3 = Profile.objects.create(
-                    name=user_3.username,
-                    email="pet@mail.ru",
-                    phone_number=user_3
-                )
+            # user_1 = User.objects.create(
+            #     phone="+79004217012",
+            #     username="Иван"
+            # )
+            # user_2 = User.objects.create(
+            #     phone="+79004216012",
+            #     username="Любовь"
+            # )
+            # user_3 = User.objects.create(
+            #     phone="+79004215012",
+            #     username="Пётр"
+            # )
+            # if not Profile.objects.filter(phone_number=user_1).exists():
+            #     profile_1 = Profile.objects.create(
+            #         name=user_1.username,
+            #         email="iv@mail.ru",
+            #         phone_number=user_1
+            #     )
+            # if not Profile.objects.filter(phone_number=user_2).exists():
+            #     profile_2 = Profile.objects.create(
+            #         name=user_2.username,
+            #         email="love@mail.ru",
+            #         phone_number=user_2
+            #     )
+            # if not Profile.objects.filter(phone_number=user_3).exists():
+            #     profile_3 = Profile.objects.create(
+            #         name=user_3.username,
+            #         email="pet@mail.ru",
+            #         phone_number=user_3
+            #     )
 
             size_1 = Sizes.objects.create(
                 title="1",
@@ -140,54 +140,54 @@ class Command(BaseCommand):
                 price="280"
             )
 
-            order_1 = Order.objects.create(
-                size=size_1,
-                form=form_1,
-                topping=topping_1,
-                berry=berries_1,
-                decor=decor_1,
-                inscription="С днём рождения",
-                client_name="Иван",
-                phonenumber="+79004217012",
-                email="iv@mail.ru",
-                address="Ленина 1",
-                delivery_datetime=timezone.make_aware(
-                    datetime.datetime(2025, 3, 2, 8, 0)
-                ),
-                total_cost="1500",
-            )
-            order_2 = Order.objects.create(
-                size=size_2,
-                form=form_2,
-                topping=topping_2,
-                berry=berries_2,
-                decor=decor_2,
-                inscription="С днём рождения",
-                client_name="Любовь",
-                phonenumber="+79004216012",
-                email="love@mail.ru",
-                address="Ленина 2",
-                delivery_datetime=timezone.make_aware(
-                    datetime.datetime(2025, 3, 1, 8, 0)
-                ),
-                total_cost="2000",
-            )
-            order_3 = Order.objects.create(
-                size=size_3,
-                form=form_3,
-                topping=topping_3,
-                berry=berries_3,
-                decor=decor_3,
-                inscription="С днём рождения",
-                client_name="Пётр",
-                phonenumber="+79004215012",
-                email="pet@mail.ru",
-                address="Ленина 3",
-                delivery_datetime=timezone.make_aware(
-                    datetime.datetime(2025, 2, 28, 8, 0)
-                ),
-                total_cost="2500",
-            )
+            # order_1 = Order.objects.create(
+            #     size=size_1,
+            #     form=form_1,
+            #     topping=topping_1,
+            #     berry=berries_1,
+            #     decor=decor_1,
+            #     inscription="С днём рождения",
+            #     client_name="Иван",
+            #     phonenumber="+79004217012",
+            #     email="iv@mail.ru",
+            #     address="Ленина 1",
+            #     delivery_datetime=timezone.make_aware(
+            #         datetime.datetime(2025, 3, 2, 8, 0)
+            #     ),
+            #     total_cost="1500",
+            # )
+            # order_2 = Order.objects.create(
+            #     size=size_2,
+            #     form=form_2,
+            #     topping=topping_2,
+            #     berry=berries_2,
+            #     decor=decor_2,
+            #     inscription="С днём рождения",
+            #     client_name="Любовь",
+            #     phonenumber="+79004216012",
+            #     email="love@mail.ru",
+            #     address="Ленина 2",
+            #     delivery_datetime=timezone.make_aware(
+            #         datetime.datetime(2025, 3, 1, 8, 0)
+            #     ),
+            #     total_cost="2000",
+            # )
+            # order_3 = Order.objects.create(
+            #     size=size_3,
+            #     form=form_3,
+            #     topping=topping_3,
+            #     berry=berries_3,
+            #     decor=decor_3,
+            #     inscription="С днём рождения",
+            #     client_name="Пётр",
+            #     phonenumber="+79004215012",
+            #     email="pet@mail.ru",
+            #     address="Ленина 3",
+            #     delivery_datetime=timezone.make_aware(
+            #         datetime.datetime(2025, 2, 28, 8, 0)
+            #     ),
+            #     total_cost="2500",
+            # )
 
             self.stdout.write(
                 self.style.SUCCESS(f"Тестовые данные успешно загружены в бд")
